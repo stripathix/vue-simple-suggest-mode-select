a55fc45bf01005dccd40b3fa06cca2dad85a3a43
Tripathi <SHATRI@barco.com>
2019-05-23 10:42:31 +0530

    first draft

---
+++ app/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+    <auto-suggest
+      id="input-live"
+      v-model="model"
+      value-attribute="id"
+      mode="select"
+      display-attribute="name"
+      :list="simpleSuggestionList"
+      @select="onSuggestSelect">
+    </auto-suggest>
+    <p v-if="selected">Selected element ({{ typeof selected }}): <pre class="selected hljs"><code v-html="selected"></code></pre></p>
+      <p v-if="model">v-model ({{ typeof model }}): <pre class="selected hljs"><code v-html="model"></code></pre></p>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import AutoSuggest from './components/AutoSuggest.vue';
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    AutoSuggest,
+  },
+  data() {
+    return {
+      selected: null,
+      model: null,
+    };
+  },
+  methods: {
+    onSuggestSelect (suggest) {
+      this.selected = suggest ? suggest.id : null;
+    },
+    simpleSuggestionList() {
+      return [
+        {name: 'Afghanistan', id: 'AF'},
+        {name: 'Åland Islands', id: 'AX'},
+        {name: 'Albania', id: 'AL'},
+        {name: 'Algeria', id: 'DZ'},
+        {name: 'American Samoa', id: 'AS'},
+        {name: 'AndorrA', id: 'AD'},
+        {name: 'Angola', id: 'AO'},
+        {name: 'Anguilla', id: 'AI'},
+        {name: 'Antarctica', id: 'AQ'},
+        {name: 'Antigua and Barbuda', id: 'AG'},
+        {name: 'Argentina', id: 'AR'},
+      ];
+    },
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ app/src/components/AutoSuggest.vue
@@ -0,0 +1,38 @@
+<template>
+  <vue-simple-suggest
+      :value="value"
+      :styles="autoCompleteStyle"
+      v-bind="$attrs"
+      v-on="$listeners"
+      autocomplete="off"
+      :filter-by-query="true">
+  </vue-simple-suggest>
+</template>
+<script>
+import 'vue-simple-suggest/dist/styles.css'
+import vueSimpleSuggest  from 'vue-simple-suggest';
+
+export default {
+  components: {
+    vueSimpleSuggest,
+  },
+  inheritAttrs: false,
+  props: {
+    value: {
+      type: String,
+      default: null,
+    },
+  },
+  data() {
+    return {
+      autoCompleteStyle: {
+        vueSimpleSuggest: 'position-relative',
+        inputWrapper: '',
+        defaultInput : 'form-control',
+        suggestions: 'dropdown-menu show',
+        suggestItem: 'dropdown-item'
+      },
+    };
+  },
+};
+</script>
